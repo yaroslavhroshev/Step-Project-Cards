260fdda9991b45ea9358522443572cf2048e2adc
Ярослав Грошев <yaroslavhroshev@gmail.com>
2023-06-22 23:21:36 +0300

    The logic of the visit creation modal window has been implemented

--- js/Classes/CreateVisitForm.js
+++ js/Classes/CreateVisitForm.js
@@ -1,7 +1,7 @@
 import Form from "./form.js";
 
 export default class CreateVisitForm extends Form {
-    constructor(title) {
+    constructor(title, changeFormFunction) {
         super(title)
         this.select = document.createElement('select');
         this.cardiologist = document.createElement('option');
@@ -9,17 +9,20 @@ export default class CreateVisitForm extends Form {
         this.dentist = document.createElement('option');
         this.emptyOption = document.createElement('option');
         this.optionsWrapper = document.createElement('div');
+        this.additionalInformSection = document.createElement('div');
 
-        this.visitPurpose = '<input type="text" placeholder="Мета візиту">';
-        this.visitDesc = '<textarea name="descOfVisit" id="desc" cols="20" rows="5"  placeholder="Короткий опис візиту"></textarea>';
+        this.visitPurpose = '<input type="text" name="visit_purpose" placeholder="Мета візиту">';
+        this.visitDesc = '<textarea name="visit_description" id="desc" cols="20" rows="5"  placeholder="Короткий опис візиту"></textarea>';
         this.urgency = `
-        <select id="prioritySelect">
+        <select name="priority" id="prioritySelect">
         <option value="">Пріоритет</option>
         <option value="high">Високий</option>
         <option value="normal">Звичайний</option>
         <option value="low">Низький</option>
         </select>`;
-        this.fullName = '<input type="text" placeholder="Прізвище Ім\'я По-батькові ">';
+        this.fullName = '<input type="text" name="full_name" placeholder="Прізвище Ім\'я По-батькові ">';
+
+        this.changeFormFunction = changeFormFunction;
     }
 
     createElement() {
@@ -27,17 +30,21 @@ export default class CreateVisitForm extends Form {
 
         this.select.classList.add('select')
         this.optionsWrapper.classList.add('options-wrapper')
+        this.additionalInformSection.classList.add('additional-inform-section')
 
         this.select.append(this.emptyOption);
         this.select.append(this.cardiologist);
         this.select.append(this.dentist);
         this.select.append(this.therapist);
-        this.optionsWrapper.append(this.select)
+        this.optionsWrapper.append(this.select);
 
         this.optionsWrapper.insertAdjacentHTML('beforeend', this.visitPurpose)
         this.optionsWrapper.insertAdjacentHTML('beforeend', this.visitDesc)
         this.optionsWrapper.insertAdjacentHTML('beforeend', this.urgency)
         this.optionsWrapper.insertAdjacentHTML('beforeend', this.fullName)
+
+        this.optionsWrapper.append(this.additionalInformSection);
+
         this.form.append(this.optionsWrapper)
 
         this.cardiologist.textContent = "Кардіолог";
@@ -49,33 +56,58 @@ export default class CreateVisitForm extends Form {
         this.therapist.value = "therapist";
         this.dentist.value = "dentist";
 
+        this.select.name = 'doctor';
+
 
     }
 
     changeFormByOption(optionValue) {
-        if (optionValue === 'cardiologist') {
+        this.additionalInformSection.innerHTML = "";
+        const additionalElement = this.changeFormFunction(optionValue);
 
-        }
-
-        if (optionValue === 'therapist') {
-
-        }
-
-        if (optionValue === 'dentist') {
-
+        if (additionalElement !== null) {
+            this.additionalInformSection.append(additionalElement);
         }
     }
 
     getOptionsValues() {
         this.select.addEventListener('change', e => {
-            // console.log(e.target.value);
-            // this.changeFormByOption(e.target.value)
-            return e.target.value
+            this.changeFormByOption(e.target.value)
         })
+    }
+
+    getSelectsValues(body) {
+        const selects = this.form.querySelectorAll('select');
+        selects.forEach(select => {
+            if (select.value === 'Оберіть лікаря') {
+                return
+            }
+            // if (select.value === "") {        // Цей код в розробці 
+            //     throw new Error('Гайки тобі') // Намагаюсь зробити поля обов'язковими для вводу
+            // }
+            body[select.name] = select.value
+        })
+        return body
+    }
+
+    getTextareaValues(body) {
+        const textareas = this.form.querySelectorAll('textarea');
+        textareas.forEach(textarea => {
+            body[textarea.name] = textarea.value
+        })
+        return body
+    }
+
+    getAllUserInfo() {
+        const inputs = this.getValues();
+        const selects = this.getSelectsValues(inputs);
+        const textareas = this.getTextareaValues(selects)
+        return textareas
     }
 
     getFormElement() {
         this.getOptionsValues()
+
         return super.getFormElement()
     }
 
--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ import LoginForm from "./Classes/loginForm.js";
 import loginFunction from "./API/loginfunction.js";
 import checkLoginToken from "./Functions/checkLoginToken.js";
 import CreateVisitForm from "./Classes/CreateVisitForm.js";
+import VisitDentist from "./Classes/VisitDentist.js";
+import VisitTherapist from "./Classes/VisitTherapist.js";
+import VisitCardiologist from "./Classes/VisitCardiologist.js";
+import postElement from "./API/postElement.js";
 
 const loginBtn = document.querySelector('#loginButton');
 
@@ -20,7 +24,7 @@ loginBtn.addEventListener('click', (e) => {
         checkLoginToken()
     };
 
-    new LoginModal(form.getFormElement(), confirmRegestration).render()
+    new LoginModal(form.getFormElement(), confirmRegestration, 'Увійти').render()
 })
 
 const addVisit = document.querySelector('#addVisitButton');
@@ -29,24 +33,37 @@ console.log(addVisit);
 
 addVisit.addEventListener('click', () => {
 
-    const form = new CreateVisitForm("Створити візит");
+    const checkOptions = (optionValue) => {
+
+        if (optionValue === 'cardiologist') {
+            const cardiologist = new VisitCardiologist("Створити візит").getAdditionalInformation();
+            return cardiologist
+        }
+
+        if (optionValue === 'therapist') {
+            const therapist = new VisitTherapist("Створити візит").getAdditionalInformation();
+            return therapist
+        }
+
+        if (optionValue === 'dentist') {
+            const dentist = new VisitDentist("Створити візит").getAdditionalInformation();
+            return dentist
+        }
+
+        return null
+    }
+
+    const form = new CreateVisitForm("Створити візит", checkOptions);
 
     const confirmRegestration = async (closerCallbackFromModal) => {
 
-        const checkOptions = (optionValue, f) => {
-
-
-
-        }
-
-        // const body = form.getValues();
-        // const { data } = await loginFunction(body)
-        // localStorage.setItem('TOKEN', data)
+        const body = form.getAllUserInfo();
+        const { data } = await postElement(body);
         closerCallbackFromModal()
-        // checkLoginToken()
+        console.log(data);
     };
 
-    new LoginModal(form.getFormElement(), confirmRegestration).render()
+    new LoginModal(form.getFormElement(), confirmRegestration, "Створити").render()
 
 })
 
