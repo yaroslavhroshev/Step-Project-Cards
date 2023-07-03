72b83308d396c3c3962a952e81173f316a09bfc9
Ярослав Грошев <yaroslavhroshev@gmail.com>
2023-07-03 10:23:38 +0300

    The main branch is clear

--- js/Functions/textHiddenFunction.js
+++
@@ -1,9 +0,0 @@
-const textHiddenFunction = () => {
-    if (document.querySelector('#visitList').querySelector('.card') !== null) {
-        document.querySelector('.no-items').classList.remove('hidden');
-    } else {
-        document.querySelector('.no-items').classList.add('hidden');
-    }
-}
-
-export default textHiddenFunction
